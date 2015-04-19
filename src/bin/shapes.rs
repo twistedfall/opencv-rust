c33a31988e304a681450f35b1d9b6e2caebbdead
Mathieu Poumeyrol <kali@zoy.org>
2015-04-19 20:40:02 +0200

    quiet a warning

--- src/bin/shapes.rs
+++ src/bin/shapes.rs
@@ -2,8 +2,6 @@ extern crate opencv_sys;
 
 use opencv_sys::*;
 
-use std::ffi::CString;
-
 #[allow(dead_code)]
 fn main() {
     let mat = core::Mat::for_dims(400,300, core::Mat::make_type(core::ChannelDepth::CV_8U, 3));
