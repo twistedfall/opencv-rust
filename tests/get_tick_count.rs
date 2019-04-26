d95fb983a3c3c905eb5658c3eaf98b346a5daf27
Pro <twisted.fall@gmail.com>
2019-04-26 19:34:59 +0200

    improve tests

--- tests/get_tick_count.rs
+++ tests/get_tick_count.rs
@@ -2,18 +2,14 @@ use opencv::core;
 
 #[test]
 fn test_primitives() {
-    let ticks = core::get_tick_count().unwrap();
-    assert!(ticks > 10000);
-    let freq = core::get_tick_frequency().unwrap();
-    assert!(freq > 1000f64);
-    let cpus = core::get_number_of_cpus().unwrap();
-    assert!(cpus > 0);
+    assert!(core::get_tick_count().unwrap() > 10000);
+    assert!(core::get_cpu_tick_count().unwrap() > 10000);
+    assert!(core::get_tick_frequency().unwrap() > 1000.);
+    assert!(core::get_number_of_cpus().unwrap() >= 1);
     core::set_use_optimized(true).unwrap();
-    let optims = core::use_optimized().unwrap();
-    assert!(optims);
+    assert!(core::use_optimized().unwrap());
     core::set_use_optimized(false).unwrap();
-    let optims = core::use_optimized().unwrap();
-    assert!(!optims);
+    assert!(!core::use_optimized().unwrap());
 }
 
 #[test]
