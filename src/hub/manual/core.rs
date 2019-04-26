7c7a9a53b146fb403bfb722746c3f5fcf83b1e4a
Pro <twisted.fall@gmail.com>
2019-04-26 23:32:07 +0200

    base for manual core

---
+++ src/hub/manual/core.rs
@@ -0,0 +1,10 @@
+macro_rules! valid_types {
+    ($trait: ident, $($rust_type: ty),+) => {
+        // todo, make sealed
+        pub trait $trait {}
+
+        $(
+            impl $trait for $rust_type {}
+        )+
+    };
+}
