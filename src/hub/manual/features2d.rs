b66e534d1e577d100afad4252d354680250e9058
Pro <twisted.fall@gmail.com>
2019-06-17 18:20:53 +0200

    code cleanup

--- src/hub/manual/features2d.rs
+++ src/hub/manual/features2d.rs
@@ -4,7 +4,7 @@ use crate::{
     types::PtrOfORB,
 };
 
-impl ORB {
+impl dyn ORB {
     pub fn default() -> Result<PtrOfORB> {
         ORB::create(500, 1.2, 8, 31, 0, 2, features2d::ORB_HARRIS_SCORE, 31, 20)
     }
