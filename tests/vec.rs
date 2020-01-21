faf2d494d94f9faf6da106badac7c3489cd4d712
Pro <twisted.fall@gmail.com>
2020-01-21 12:25:14 +0100

    add some more tests

--- tests/vec.rs
+++ tests/vec.rs
@@ -1,11 +1,16 @@
-use opencv::core;
+use opencv::core::Vec3b;
 
 #[test]
 fn vec() {
-    let mut a = core::Vec3b::default();
+    let mut a = Vec3b::default();
     assert_eq!(a[0], 0);
     assert_eq!(a.len(), 3);
 
     a[1] = 2;
     assert_eq!(a[1], 2);
+}
+
+#[test]
+fn vec_deref() {
+    assert_eq!(vec![10, 20, 30], Vec3b::from([10, 20, 30]).to_vec());
 }
