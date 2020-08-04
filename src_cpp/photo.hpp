fc8025e429e42d41b613e7560417a76a4b4eacf9
Pro <twisted.fall@gmail.com>
2020-08-04 20:32:47 +0200

    Include more CUDA functions

---
+++ src_cpp/photo.hpp
@@ -0,0 +1,3 @@
+#include "ocvrs_common.hpp"
+#include <opencv2/photo.hpp>
+#include <opencv2/photo/cuda.hpp>
---
+++ src_cpp/xfeatures2d.hpp
@@ -0,0 +1,3 @@
+#include "ocvrs_common.hpp"
+#include <opencv2/xfeatures2d.hpp>
+#include <opencv2/xfeatures2d/cuda.hpp>
