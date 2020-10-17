329de7af8f60ed1b4eb6c30f0dbd72e1b10197ea
Pro <twisted.fall@gmail.com>
2020-10-17 11:51:12 +0200

    Fix opencl tests when opencl is not enabled

--- ci/test-proj-clang-runtime/src/main.rs
+++ ci/test-proj-clang-runtime/src/main.rs
@@ -2,5 +2,5 @@ use opencv::prelude::*;
 
 fn main() {
 	let m = Mat::default().unwrap();
-	println!("{}", opencv::core::type_to_str(m.typ().unwrap()).unwrap());
+	println!("{}", m.typ().unwrap());
 }
