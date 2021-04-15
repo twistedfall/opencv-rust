36ec9139aae364ce508a0370dd193df3465a1917
Pro <twisted.fall@gmail.com>
2021-04-15 07:27:27 +0200

    More fine-grained header inclusion

--- src_cpp/core.hpp
+++ src_cpp/core.hpp
@@ -5,10 +5,17 @@
 #include <opencv2/core/va_intel.hpp>
 #include <opencv2/core/cuda.hpp>
 #include <opencv2/core/opengl.hpp>
-#if !(CV_VERSION_MAJOR == 3 && CV_VERSION_MINOR == 2)
-	#include <opencv2/core/async.hpp>
+#if (CV_VERSION_MAJOR == 3 && CV_VERSION_MINOR == 4 && CV_VERSION_REVISION >= 4) /* 3.4.4+ */ \
+	|| (CV_VERSION_MAJOR == 4) /* 4.0+ */
 	#include <opencv2/core/bindings_utils.hpp>
+#endif
+#if !(CV_VERSION_MAJOR == 3 && CV_VERSION_MINOR == 2)
 	#include <opencv2/core/utils/logger.hpp>
+#endif
+#if (CV_VERSION_MAJOR == 3 && CV_VERSION_MINOR == 4 && CV_VERSION_REVISION >= 7) /* 3.4.7+ */ \
+	|| (CV_VERSION_MAJOR == 4 && CV_VERSION_MINOR == 1 && CV_VERSION_REVISION >= 1) /* 4.1.1+ */ \
+	|| (CV_VERSION_MAJOR == 4 && CV_VERSION_MINOR >= 2) /* 4.2+ */
+	#include <opencv2/core/async.hpp>
 #endif
 
 namespace cv {
