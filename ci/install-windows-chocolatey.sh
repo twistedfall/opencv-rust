c56b2a1df306ec2416b2c824b921e9899127f3a0
Pro <twisted.fall@gmail.com>
2021-07-21 13:57:25 +0200

    Fix vcpkg build

--- ci/install-windows-chocolatey.sh
+++ ci/install-windows-chocolatey.sh
@@ -2,5 +2,5 @@
 
 set -vex
 
-choco install -y llvm --version 12.0.0
+choco install -y llvm --version 12.0.1
 choco install -y opencv --version "$CHOCO_OPENCV_VERSION"
--- ci/install-windows-vcpkg.sh
+++ ci/install-windows-vcpkg.sh
@@ -2,6 +2,7 @@
 
 set -vex
 
+rm -rf "$VCPKG_INSTALLATION_ROOT"
 export VCPKG_ROOT="$HOME/build/vcpkg"
 if [[ -e "$VCPKG_ROOT" && ! -e "$VCPKG_ROOT/.git" ]]; then
 	rm -rf "$VCPKG_ROOT"
@@ -12,15 +13,15 @@ fi
 pushd "$VCPKG_ROOT"
 git fetch --all --prune --tags
 git checkout .
-git checkout 2021.05.12
-cmd "/C bootstrap-vcpkg.bat -disableMetrics"
+git checkout 025e564979cc01d0fbc5c920aa8a36635efb01bb
+./bootstrap-vcpkg.sh -disableMetrics
 #./vcpkg integrate install
-echo "set(VCPKG_BUILD_TYPE release)" >> triplets/x64-windows.cmake
-echo "set(VCPKG_BUILD_TYPE release)" >> triplets/x64-windows-static.cmake
-echo "set(VCPKG_BUILD_TYPE release)" >> triplets/x86-windows.cmake
+echo "set(VCPKG_BUILD_TYPE release)" >> "$VCPKG_ROOT/triplets/x64-windows.cmake"
+echo "set(VCPKG_BUILD_TYPE release)" >> "$VCPKG_ROOT/triplets/x64-windows-static.cmake"
+echo "set(VCPKG_BUILD_TYPE release)" >> "$VCPKG_ROOT/triplets/x86-windows.cmake"
 export VCPKG_DEFAULT_TRIPLET=x64-windows
 #./vcpkg install llvm  # takes very long time
-choco install -y llvm --version 12.0.0
-./vcpkg upgrade --no-dry-run
-./vcpkg install --recurse "opencv${VCPKG_OPENCV_VERSION}[contrib,nonfree]"
+choco install -y llvm --version 12.0.1
+"$VCPKG_ROOT/vcpkg" upgrade --no-dry-run
+"$VCPKG_ROOT/vcpkg" install --recurse "opencv${VCPKG_OPENCV_VERSION}[contrib,nonfree]"
 popd
