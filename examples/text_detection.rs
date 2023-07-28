9c6d6204df87c980b80f2796c772cf1fdeb4ea71
Pro <twisted.fall@gmail.com>
2023-07-28 21:58:33 +0200

    Add CommandLineParser to text_detection example

--- examples/text_detection.rs
+++ examples/text_detection.rs
@@ -2,38 +2,66 @@
 //! Check the source cpp file for where to get the NN files.
 //! This example requires at least OpenCV 4.5.1.
 
+use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::path::PathBuf;
 
-use opencv::core::{Point2f, Scalar, Size};
+use dnn::{TextDetectionModel_EAST, TextRecognitionModel};
+use opencv::core::{CommandLineParser, Point, Point2f, Scalar, Size, Vector};
 use opencv::prelude::*;
-use opencv::types::{VectorOfPoint2f, VectorOfString, VectorOfVectorOfPoint};
 use opencv::{core, dnn, highgui, imgproc, videoio};
+use videoio::VideoCapture;
 
 type Result<T, E = Box<dyn Error>> = std::result::Result<T, E>;
 
-fn main() -> Result<()> {
+const KEYS: &str = concat!(
+	"{ help  h              | | Print help message. }",
+	"{ input i              | | Path to input image or video file. Skip this argument to capture frames from a camera.}",
+	"{ detModel dmp         | | Path to a binary .pb file contains trained detector network.}",
+	"{ width                | 320 | Preprocess input image by resizing to a specific width. It should be multiple by 32. }",
+	"{ height               | 320 | Preprocess input image by resizing to a specific height. It should be multiple by 32. }",
+	"{ thr                  | 0.5 | Confidence threshold. }",
+	"{ nms                  | 0.4 | Non-maximum suppression threshold. }",
+	"{ recModel rmp         | | Path to a binary .onnx file contains trained CRNN text recognition model. ",
+	"Download links are provided in doc/tutorials/dnn/dnn_text_spotting/dnn_text_spotting.markdown}",
+	"{ RGBInput rgb         |0| 0: imread with flags=IMREAD_GRAYSCALE; 1: imread with flags=IMREAD_COLOR. }",
+	"{ vocabularyPath vp    | alphabet_36.txt | Path to benchmarks for evaluation. ",
+	"Download links are provided in doc/tutorials/dnn/dnn_text_spotting/dnn_text_spotting.markdown}",
+);
+
+pub fn main() -> Result<()> {
+	let args = env::args().collect::<Vec<_>>();
+	let args = args.iter().map(|arg| arg.as_str()).collect::<Vec<_>>();
+	let mut parser = CommandLineParser::new(i32::try_from(args.len()).expect("Too many arguments"), &args, KEYS)?;
+	parser.about(
+		"Use this script to run TensorFlow implementation (https://github.com/argman/EAST) of \
+		EAST: An Efficient and Accurate Scene Text Detector (https://arxiv.org/abs/1704.03155v2)",
+	)?;
+	if args.len() == 1 || parser.has("help")? {
+		parser.print_message()?;
+		return Ok(());
+	}
+
 	// Parameters.
-	let conf_threshold = 0.5;
-	let nms_threshold = 0.4;
-	let width = 320;
-	let height = 320;
-	let imread_rgb = false;
-	let det_model_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("frozen_east_text_detection.pb");
-	let rec_model_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("crnn.onnx");
-	let voc_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("alphabet_36.txt");
+	let conf_threshold = parser.get_f64("thr", true)? as f32;
+	let nms_threshold = parser.get_f64("nms", true)? as f32;
+	let width = parser.get_i32("width", true)?;
+	let height = parser.get_i32("height", true)?;
+	let imread_rgb = parser.get_bool("RGBInput", true)?;
+	let det_model_path = parser.get_str("detModel", true)?;
+	let rec_model_path = parser.get_str("recModel", true)?;
+	let voc_path = parser.get_str("vocabularyPath", true)?;
 
 	// Load networks.
-	let mut detector = dnn::TextDetectionModel_EAST::from_file(det_model_path.to_str().unwrap(), "")?;
+	let mut detector = TextDetectionModel_EAST::from_file(&det_model_path, "")?;
 	detector
 		.set_confidence_threshold(conf_threshold)?
 		.set_nms_threshold(nms_threshold)?;
-	let mut recognizer = dnn::TextRecognitionModel::from_file(rec_model_path.to_str().unwrap(), "")?;
+	let mut recognizer = TextRecognitionModel::from_file(&rec_model_path, "")?;
 
 	// Load vocabulary
-	let mut vocabulary = VectorOfString::new();
+	let mut vocabulary = Vector::<String>::new();
 	let voc_file = BufReader::new(File::open(voc_path)?);
 	for voc_line in voc_file.lines() {
 		vocabulary.push(&voc_line?);
@@ -54,8 +82,8 @@ fn main() -> Result<()> {
 	detector.set_input_params(det_scale, det_input_size, det_mean, swap_rb, false)?;
 
 	// Open a video file or an image file or a camera stream.
-	let mut cap = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
-	let open_success = videoio::VideoCapture::is_opened(&cap)?;
+	let mut cap = VideoCapture::new(0, videoio::CAP_ANY)?;
+	let open_success = VideoCapture::is_opened(&cap)?;
 	if !open_success {
 		panic!("Unable to open default camera!");
 	}
@@ -69,7 +97,7 @@ fn main() -> Result<()> {
 		}
 		println!("Frame size: {:?}", frame.size()?);
 		// Detection
-		let mut det_results = VectorOfVectorOfPoint::new();
+		let mut det_results = Vector::new();
 		detector.detect(&frame, &mut det_results)?;
 
 		if !det_results.is_empty() {
@@ -81,9 +109,9 @@ fn main() -> Result<()> {
 			} else {
 				None
 			};
-			let mut contours = VectorOfVectorOfPoint::new();
+			let mut contours = Vector::<Vector<Point>>::new();
 			for quadrangle in &det_results {
-				let mut quadrangle_2f = VectorOfPoint2f::new();
+				let mut quadrangle_2f = Vector::<Point2f>::new();
 				for pt in &quadrangle {
 					quadrangle_2f.push(Point2f::new(pt.x as f32, pt.y as f32))
 				}
@@ -128,12 +156,7 @@ fn four_points_transform(frame: &Mat, vertices: &[Point2f]) -> Result<Mat> {
 		Point2f::new((output_size.width - 1) as f32, 0.),
 		Point2f::new((output_size.width - 1) as f32, (output_size.height - 1) as f32),
 	];
-	opencv::opencv_branch_4! {
-		let rotation_matrix = imgproc::get_perspective_transform_slice(vertices, &target_vertices, core::DECOMP_LU)?;
-	}
-	opencv::not_opencv_branch_4! {
-		let rotation_matrix = imgproc::get_perspective_transform_slice(vertices, &target_vertices)?;
-	}
+	let rotation_matrix = imgproc::get_perspective_transform_slice(vertices, &target_vertices, core::DECOMP_LU)?;
 	let mut out = Mat::default();
 	imgproc::warp_perspective(
 		frame,
