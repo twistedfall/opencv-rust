31ec40eaa7e7bede737ca5f2c3fbac47724df9eb
fzyzcjy <ch271828n@outlook.com>
2023-10-02 21:26:59 +0800

    Revert "hack remove SparseMatConstIterator__operatorSS"
    This reverts commit d2c0434502958aeff9fb91899bc655abd62552a6.

--- binding-generator/src/settings.rs
+++ binding-generator/src/settings.rs
@@ -549,9 +549,8 @@ pub static FUNC_RENAME: Lazy<HashMap<&str, &str>> = Lazy::new(|| {
 pub static FUNC_CFG_ATTR: Lazy<HashMap<&str, (&str, &str)>> = Lazy::new(|| {
 	HashMap::from([
 		// ### core ###
-		// NOTE MODIFIED HACK
-		// ("cv_SparseMatConstIterator_operatorSS", ("not(target_os = \"windows\")", "!defined(OCVRS_TARGET_OS_WINDOWS)")),
-		// ("cv__OutputArray__OutputArray_const_vectorLGpuMatGR", ("not(target_os = \"windows\")", "!defined(OCVRS_TARGET_OS_WINDOWS)")),
+		("cv_SparseMatConstIterator_operatorSS", ("not(target_os = \"windows\")", "!defined(OCVRS_TARGET_OS_WINDOWS)")),
+		("cv__OutputArray__OutputArray_const_vectorLGpuMatGR", ("not(target_os = \"windows\")", "!defined(OCVRS_TARGET_OS_WINDOWS)")),
 
 		// ### imgproc ###
 		("cv_getRotationMatrix2D__Point2f_double_double", ("not(target_os = \"windows\")", "!defined(OCVRS_TARGET_OS_WINDOWS)")),
