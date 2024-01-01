fe89f79cad6a3aa237a114bde1eddab04eab6b85
Pro <twisted.fall@gmail.com>
2024-01-01 17:06:46 +0100

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![allow(broken_intra_doc_links)]
 
-pub use cond_macros::*;
 pub use error::{Error, Result};
 
 pub use crate::opencv::hub::*;
@@ -30,23 +29,18 @@ pub mod platform_types {
 pub(crate) mod mod_prelude_sys {
 	pub use std::ffi::{c_char, c_void};
 
-	pub use crate::{
-		platform_types::*,
-		traits::{Boxed, OpenCVType, OpenCVTypeArg, OpenCVTypeExternContainer},
-	};
+	pub use crate::platform_types::*;
+	pub use crate::traits::{Boxed, OpenCVType, OpenCVTypeArg, OpenCVTypeExternContainer};
 }
 
 /// Prelude for generated modules and types
 pub(crate) mod mod_prelude {
+	pub use crate::hub_prelude::*;
+	pub use crate::mod_prelude_sys::*;
 	pub use crate::{
-		boxed_cast_base, boxed_cast_descendant,
-		core::{CV_MAKETYPE, CV_MAKE_TYPE},
-		extern_arg_send, extern_container_send, extern_receive, extern_send,
-		hub_prelude::*,
-		input_array_ref_forward,
-		mod_prelude_sys::*,
-		opencv_type_boxed, opencv_type_enum, opencv_type_simple, output_array_ref_forward, ptr_cast_base, ptr_extern,
-		ptr_extern_ctor, tuple_extern, vector_copy_non_bool, vector_extern, vector_non_copy_or_bool, Error, Result,
+		boxed_cast_base, boxed_cast_descendant, extern_arg_send, extern_container_send, extern_receive, extern_send,
+		input_array_ref_forward, opencv_type_boxed, opencv_type_enum, opencv_type_simple, output_array_ref_forward, ptr_cast_base,
+		ptr_extern, ptr_extern_ctor, tuple_extern, vector_copy_non_bool, vector_extern, vector_non_copy_or_bool, Result,
 	};
 }
 
