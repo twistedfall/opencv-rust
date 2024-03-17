de04c3bc6949130572202fefb47464e0e9889646
modelflat <zhykreg@gmail.com>
2024-03-17 22:22:23 +0100

    [wip] add directx interop

--- src/manual/core.rs
+++ src/manual/core.rs
@@ -1,5 +1,7 @@
 pub use affine3::*;
 pub use data_type::*;
+#[cfg(feature = "directx")]
+pub use directx::*;
 pub use input_output_array::*;
 pub use mat::*;
 pub use mat_ops::*;
@@ -18,6 +20,8 @@ pub use CV_MAKETYPE as CV_MAKE_TYPE;
 
 mod affine3;
 mod data_type;
+#[cfg(feature = "directx")]
+mod directx;
 mod gpumat;
 mod input_output_array;
 mod mat;
