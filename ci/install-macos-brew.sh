081ec1dfccb4cbb9942dc1d34083d4b4fe7ea284
Pro <twisted.fall@gmail.com>
2026-07-23 16:59:13 +0200

    Fix macos brew CI

--- ci/install-macos-brew.sh
+++ ci/install-macos-brew.sh
@@ -4,6 +4,12 @@ set -xeu
 
 brew list --versions
 brew -v update
+
+# fixes the install on 2026-07-23
+rm -rf /opt/homebrew/lib/ruby/gems/3.3.0/doc/rubygems-4.0.16
+rm -rf /opt/homebrew/lib/ruby/gems/3.3.0/gems/bundler-4.0.16
+rm -f /opt/homebrew/lib/ruby/gems/3.3.0/specifications/default/bundler-4.0.16.gemspec
+
 brew upgrade --force --display-times
 brew list --versions
 brew -v install --force --display-times opencv"$BREW_OPENCV_VERSION"
